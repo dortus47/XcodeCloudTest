6f6cc38e204227cde5ced124173ef4651133eaef
dortus47 <dortus47@naver.com>
2022-10-29 17:08:58 +0900

    프로젝트 임포트

---
+++ XcodeCloudTest/XcodeCloudTest/XcodeCloudTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTestApp.swift
+//  XcodeCloudTest
+//
+//  Created by 장은석 on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
